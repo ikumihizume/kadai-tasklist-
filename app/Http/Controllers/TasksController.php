056543842ebe499a0b4a2d9d8a31aca740f04ca1
ikumi.hizume <ikumihizume@gmail.com>
2022-10-08 14:13:55 +0000

    fix.destroy

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -15,12 +15,21 @@ class TasksController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        $data = [];
+        if (\Auth::check()) { // 認証済みの場合
+            // 認証済みユーザを取得
+            $user = \Auth::user();
+            // ユーザの投稿の一覧を作成日時の降順で取得,tasklists()であってるかなあ、、tasks()maybe
+            $tasks = $user->tasklists()->orderBy('created_at', 'desc')->paginate(10);
 
-        // タスク一覧ビューでそれを表示
-        return view('tasks.index', [
-            'tasks' => $tasks,
-        ]);
+            $data = [
+                'user' => $user,
+                'tasklists' => $tasks,
+            ];
+        }
+
+        // Welcomeビューでそれらを表示
+        return view('welcome', $data);
     }
 
     /**
@@ -48,18 +57,27 @@ class TasksController extends Controller
     {
          // バリデーション
         $request->validate([
-            'status' => 'required|max:10',   // 追加
+            'status' => 'required|max:10',   
             'content' => 'required|max:255',
         ]);
         
          // タスクを作成
-        $task = new Task;
-        $task->status = $request->status;    // 追加
-        $task->content = $request->content;
-        $task->save();
+        //$task = new Task;
+        //$task->status = $request->status;    // 追加
+        //$task->content = $request->content;
+        //$task->save();
 
         // トップページへリダイレクトさせる
-        return redirect('/');
+        //return redirect('/');
+        
+        // この辺がなんとも怪しい認証済みユーザ（閲覧者）の投稿として作成（リクエストされた値をもとに作成）
+        $request->user()->tasklists()->create([
+            'status' => $request->status,
+            'content' => $request->content,
+        ]);
+
+        // 前のURLへリダイレクトさせる
+        return back();
     }
 
     /**
@@ -130,10 +148,23 @@ class TasksController extends Controller
     public function destroy($id)
     {
         // idの値でメッセージを検索して取得
-        $task = Task::findOrFail($id);
+        //$task = Task::findOrFail($id);
         // メッセージを削除
-        $task->delete();
+        //$task->delete();
 
+        // トップページへリダイレクトさせる
+        //return redirect('/');
+        
+        // idの値で投稿を検索して取得
+        $tasklist = \App\Task::findOrFail($id);
+
+        // 認証済みユーザ（閲覧者）がその投稿の所有者である場合は、投稿を削除
+        if (\Auth::id() === $tasklist->user_id) {
+            $tasklist->delete();
+        }
+
+        // 前のURLへリダイレクトさせる
+        //return back();
         // トップページへリダイレクトさせる
         return redirect('/');
     }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,18 @@
 
 @section('content')
     @if (Auth::check())
-        {{ Auth::user()->name }}
+        <div class="col-sm-8">
+                {{-- 投稿一覧 --}}
+                @include('tasks.index')
+        </div>
     @else
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the Microposts</h1>
+                <h1>Welcome to the tasklists</h1>
                 {{-- ユーザ登録ページへのリンク --}}
                 {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
     @endif
 @endsection
+
